34d78f21826db39f4a31664792ef3c2a826c0796
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-10-13 13:01:41 +0530

    drawing axis

--- main.cpp
+++ main.cpp
@@ -4,14 +4,24 @@
 using namespace std;
 
 int main() {
-
-	Vector2 position = Vector2{0, 0};
-	Vector2 size = Vector2{100, 100};
-
 	InitWindow(800, 800, "Hello from c++ raylib");
+	float width = GetScreenWidth();
+	float height = GetScreenHeight();
+	float offset = 200;
+
+	Vector2 center = Vector2{width/2,height/2};
+	Vector2 endA = Vector2{width/2-offset, width/2};
+	Vector2 endB = Vector2{width/2, width/2-offset};
+	Vector2 endC = Vector2{width/2+offset, width/2};
+	Vector2 endD = Vector2{width/2, width/2+offset};
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		DrawRectangleV(position, size, RED);
+		DrawLineEx(center, endA, 1.0, RED);
+		DrawLineEx(center, endB, 1.0, BLUE);
+		DrawLineEx(center, endC, 1.0, GREEN);
+		DrawLineEx(center, endD, 1.0, YELLOW);
+
 		EndDrawing();
 	}
 	CloseWindow();
