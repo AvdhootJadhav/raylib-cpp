42817d1999b1ffd6f74d09e355f5e95df583f27a
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-10-20 14:52:37 +0530

    drawing snowflake

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
 #include <bits/stdc++.h>
 #include "raylib.h"
+#include "raymath.h"
+
+#define maxDepth 3
+#define branchCount 6
+#define angle 2*3.14/branchCount
 
 using namespace std;
 
+const vector<Color> color = {BLUE, RAYWHITE, DARKBLUE};
+const int colorSet = color.size();
+
+void drawSnowflake(int depth, Vector2 origin, float length, float thickness) {
+	if (depth == maxDepth) {
+		return;
+	}
+	for (auto i=1;i<=branchCount; i++) {
+		Vector2 dest = Vector2{};
+		dest.x = origin.x + cos(angle*i)*length;
+		dest.y = origin.y + sin(angle*i)*length;
+		DrawLineEx(origin, dest, thickness, color[(depth%colorSet)]);
+		drawSnowflake(depth+1, dest, length*0.5, thickness*0.5);
+	}
+}
+
 int main() {
-	InitWindow(800, 800, "Hello from c++ raylib");
+	InitWindow(800, 800, "SnowFlake");
 	float width = GetScreenWidth();
 	float height = GetScreenHeight();
-	float offset = 200;
+	float length = 100;
+	float thickness = 10;
 
-	Vector2 center = Vector2{width/2,height/2};
-	Vector2 endA = Vector2{width/2-offset, width/2};
-	Vector2 endB = Vector2{width/2, width/2-offset};
-	Vector2 endC = Vector2{width/2+offset, width/2};
-	Vector2 endD = Vector2{width/2, width/2+offset};
-
+	Vector2 origin = Vector2{width/2, height/2};
 	while (!WindowShouldClose()) {
 		BeginDrawing();
-		DrawLineEx(center, endA, 1.0, RED);
-		DrawLineEx(center, endB, 1.0, BLUE);
-		DrawLineEx(center, endC, 1.0, GREEN);
-		DrawLineEx(center, endD, 1.0, YELLOW);
-
+		drawSnowflake(0, origin, length, thickness);
 		EndDrawing();
 	}
 	CloseWindow();
